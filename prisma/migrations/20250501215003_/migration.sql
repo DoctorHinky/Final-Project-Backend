04abac5eecd08d4f6a899fdfb767c054ab82ec45
Norman <norman.tetzlaff@gmail.com>
2025-05-02 23:11:23 +0200

    fixed an unknown issue with generate output of prisma

---
+++ prisma/migrations/20250501215003_/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "ROLE" AS ENUM ('CHILD', 'PARENT', 'EDUCATOR', 'ADULT');
+
+-- CreateTable
+CREATE TABLE "Family" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "deletedBy" INTEGER,
+    "deleteReason" TEXT,
+    "deactivated" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Family_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "secondName" TEXT,
+    "lastName" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+    "birthdate" TIMESTAMP(3),
+    "role" "ROLE" NOT NULL DEFAULT 'CHILD',
+    "hubId" INTEGER,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "online" BOOLEAN NOT NULL DEFAULT false,
+    "lastTimeOnline" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "deletedBy" INTEGER,
+    "deleteReason" TEXT,
+    "deactivated" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Family_email_key" ON "Family"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Family_phone_key" ON "Family"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_hubId_fkey" FOREIGN KEY ("hubId") REFERENCES "Family"("id") ON DELETE SET NULL ON UPDATE CASCADE;
