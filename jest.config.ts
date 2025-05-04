8bd2b6172840bd49423dc5c29f8f6b912fa0a72c
Norman <norman.tetzlaff@gmail.com>
2025-05-05 00:45:36 +0200

    test data added auth test passed

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+// jest.config.ts
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+};
