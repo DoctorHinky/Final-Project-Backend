5a2caa780adff8776c530e82e4ed86d659e68444
Norman <norman.tetzlaff@gmail.com>
2025-05-07 14:30:48 +0200

    added the newst version of authcontroller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { hashPassword, verifyPassword } from './utils/password.utils';
 import { RegisterDto } from './dto/auth.register.dto';
-import { UserRoles } from '@prisma/client';
+import { UserRoles, Prisma } from '@prisma/client';
 import { LoginDto } from './dto/auth.login.dto';
 
 @Injectable()
@@ -12,51 +16,50 @@ export class AuthService {
   async register(user: RegisterDto) {
     const { username, firstname, lastname, birthdate, email, phone, password } =
       user;
-    // check if the User already exists
-    const emailInUse = await this.prisma.user.findFirst({
-      where: { email: email },
-    });
-    const phoneInUse = await this.prisma.user.findFirst({
-      where: { phone: phone },
-    });
-    if (emailInUse || phoneInUse) {
-      throw new BadRequestException(
-        'This phone number or email is already in use',
-      );
+    try {
+      user.role = user.role === 'CHILD' ? UserRoles.CHILD : UserRoles.ADULT;
+      // create the User
+      const hashedPassword = await hashPassword(password);
+      user.password = hashedPassword;
+
+      const newUser = await this.prisma.user.create({
+        data: {
+          username,
+          firstname,
+          lastname,
+          birthdate: new Date(birthdate),
+          role: user.role as UserRoles,
+          email,
+          phone,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+          username: true,
+          firstname: true,
+          lastname: true,
+          birthdate: true,
+          role: true,
+          email: true,
+          phone: true,
+          password: false,
+        },
+      });
+      return {
+        message: 'new User is created',
+        user: newUser,
+      };
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException(
+            'And user with this some of choosen credentials already exists',
+          );
+        } else {
+          throw new Error('Unknow error: ' + err.message);
+        }
+      }
     }
-    user.role = user.role === 'CHILD' ? UserRoles.CHILD : UserRoles.ADULT;
-    // create the User
-    const hashedPassword = await hashPassword(password);
-    user.password = hashedPassword;
-
-    const newUser = await this.prisma.user.create({
-      data: {
-        username,
-        firstname,
-        lastname,
-        birthdate: new Date(birthdate),
-        role: user.role as UserRoles,
-        email,
-        phone,
-        password: hashedPassword,
-      },
-      select: {
-        id: true,
-        username: true,
-        firstname: true,
-        lastname: true,
-        birthdate: true,
-        role: true,
-        email: true,
-        phone: true,
-        password: false,
-      },
-    });
-
-    return {
-      message: 'new User is created',
-      user: newUser,
-    };
   }
   async login(user: LoginDto) {
     const { username, email, password } = user;
@@ -66,11 +69,11 @@ export class AuthService {
         OR: [{ username: username }, { email: email }],
       },
     });
-    if (!dbUser) throw new BadRequestException('Invalid credentials');
+    if (!dbUser) throw new UnauthorizedException('Invalid credentials');
 
     const passwordCheck = await verifyPassword(password, dbUser.password);
 
-    if (!passwordCheck) throw new BadRequestException('Invalid credentials');
+    if (!passwordCheck) throw new UnauthorizedException('Invalid credentials');
 
     return 'hallo';
   }
