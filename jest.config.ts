99b1c00ace4ae770803e76493d5a2f11c040314e
Norman <norman.tetzlaff@gmail.com>
2025-05-07 11:44:50 +0200

    change on package.json and gitignore for testing purpose

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,13 @@
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
-  testRegex: '.*\\.e2e-spec\\.ts$',
+  testRegex: '.*\\.spec\\.ts$',
+  testPathIgnorePatterns: ['/node_modules/', '/test/app.e2e-spec.ts'],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
