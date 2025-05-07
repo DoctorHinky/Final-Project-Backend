0caf7fdccf7b86adecfdffdf1e1e860ed153b711
Norman <norman.tetzlaff@gmail.com>
2025-05-07 11:50:29 +0200

    added frontend port in .env.test

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: `http://localhost:${process.env.PORT}`, // erlaubt nur diese Domain
+    origin: `http://localhost:${process.env.FRONTEND_PORT}`, // erlaubt nur diese Domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
