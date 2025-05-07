032ab2205b87bb45346ee882c20e67330aca0edf
Norman <norman.tetzlaff@gmail.com>
2025-05-07 22:18:49 +0200

    changed the id to uuid and use the build in config model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { Authmodule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [Authmodule, UserModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    Authmodule,
+    UserModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+<<<<<<< Updated upstream
   constructor() {
     // the folling line is not a comment, it deacativates the eslint rule but only for the next line
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+=======
+  constructor(config: ConfigService) {
+>>>>>>> Stashed changes
     super({
       datasources: {
         db: {
-          url: process.env.DATABASE_URL as string,
+          url: config.get('DATABASE_URL'),
         },
       },
     });
